5a2db3f2a9963573fcf441d02b498200a213d54d
TBarsukova <pilot_tanti@mail.ru>
2022-02-23 21:39:52 +0300

    Задание №4: Перевод с unittest на py.test и реализация фикстуры для контактов

--- application.py
+++ application.py
@@ -51,7 +51,26 @@ class Application:
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
+    def return_to_home_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home").click()
+
+    def create_contact(self, contact):
+        wd = self.wd
+        # init_contact_creation
+        wd.find_element_by_link_text("add new").click()
+        # fill_contact_form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        # submit_contact_creation
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
     def destroy(self):
         self.wd.quit()
-
-
