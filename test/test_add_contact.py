293e3a58f49c5c29577de4cb6ea5accd1421daf8
TBarsukova <pilot_tanti@mail.ru>
2022-02-23 23:20:45 +0300

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,15 +13,15 @@ def app(request):
     
 def test_add_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(first_name="qwe", last_name="qwerty", address="qwe123"))
     app.return_to_home_page()
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(first_name="", last_name="", address=""))
     app.return_to_home_page()
-    app.logout()
+    app.session.logout()
