ea954dd6b9c324912142366dc6d92aed9b5925a5
TBarsukova <pilot_tanti@mail.ru>
2022-08-24 23:35:29 +0600

    Задание 21

--- fixture/db.py
+++ fixture/db.py
@@ -26,10 +26,10 @@ class DbFixture:
             for row in cursor:
                 result.append(
                     Group(
-                        id=str(row[0]), 
-                        name=row[1], 
-                        header=row[2], 
-                        footer=row[3]
+                        id=str(row[0]) or None, 
+                        name=row[1] or None, 
+                        header=row[2] or None, 
+                        footer=row[3] or None
                     )
                 )
         return result
@@ -42,18 +42,18 @@ class DbFixture:
             for row in cursor:
                 result.append(
                     Contact(
-                        id=str(row[0]), 
-                        first_name=row[1], 
-                        middle_name=row[2], 
-                        last_name=row[3],
-                        address=row[4], 
-                        home=row[5],
-                        mobile=row[6], 
-                        work=row[7],
-                        phone2=row[8],
-                        email=row[9],
-                        email2=row[10], 
-                        email3=row[11],
+                        id=str(row[0]) or None, 
+                        first_name=row[1] or None, 
+                        middle_name=row[2] or None, 
+                        last_name=row[3] or None,
+                        address=row[4] or None, 
+                        home=row[5] or None,
+                        mobile=row[6] or None, 
+                        work=row[7] or None,
+                        phone2=row[8] or None,
+                        email=row[9] or None,
+                        email2=row[10] or None, 
+                        email3=row[11] or None,
                     )
                 )
         return result
--- test/test_contact_details.py
+++ test/test_contact_details.py
@@ -9,12 +9,24 @@ def test_details_on_home_page(app):
     contact_from_edit_page = app.contact.get_contact_from_edit_page(index)
     contact_from_edit_page.all_phones = join_phones(contact_from_edit_page)
     contact_from_edit_page.all_emails = join_emails(contact_from_edit_page)
-    assert contact_from_home_page.all_phones == contact_from_edit_page.all_phones
-    assert contact_from_home_page.all_emails == contact_from_edit_page.all_emails
-    assert contact_from_home_page.last_name == contact_from_edit_page.last_name
-    assert contact_from_home_page.first_name == contact_from_edit_page.first_name
-    assert contact_from_home_page.address == contact_from_edit_page.address
+    assert clean(contact_from_home_page.all_phones) == clean(contact_from_edit_page.all_phones)
+    assert clean(contact_from_home_page.all_emails) == clean(contact_from_edit_page.all_emails)
+    assert clean(contact_from_home_page.last_name) == clean(contact_from_edit_page.last_name)
+    assert clean(contact_from_home_page.first_name) == clean(contact_from_edit_page.first_name)
+    assert clean(contact_from_home_page.address) == clean(contact_from_edit_page.address)
 
+def test_details_on_home_page(app, db):
+    contacts_from_home_page = app.contact.get_contact_list()
+    contacts_from_db = db.get_contact_list()
+    for contact in contacts_from_db:
+        contact.all_phones = join_phones(contact)
+        contact.all_emails = join_emails(contact)
+    for contact_from_db, contact_from_home_page in zip(sorted(contacts_from_home_page), sorted(contacts_from_db)):
+        assert clean(contact_from_home_page.all_phones) == clean(contact_from_db.all_phones)
+        assert clean(contact_from_home_page.all_emails) == clean(contact_from_db.all_emails)
+        assert clean(contact_from_home_page.last_name) == clean(contact_from_db.last_name)
+        assert clean(contact_from_home_page.first_name) == clean(contact_from_db.first_name)
+        assert clean(contact_from_home_page.address) == clean(contact_from_db.address)
 
 def test_details_on_view_page(app):
     contacts = app.contact.get_contact_list()
